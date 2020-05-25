5da7168fd71da230192f9592baf52ff9a6318169
jflyhak <1505811266@qq.com>
2020-05-25 09:37:37 +0800

    2020/5/259:37

---
+++ miniprogram/pages/orders/orders.js
@@ -0,0 +1,74 @@
+// miniprogram/pages/orders/orders.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    accounts: ["京东", "韵达", "申通"],
+        accountIndex: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  bindAccountChange: function(e) {
+    console.log('picker account 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+        accountIndex: e.detail.value
+    })
+}
+})
