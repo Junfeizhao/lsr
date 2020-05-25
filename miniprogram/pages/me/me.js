abc496344c14493b99227b987f225f2774f70b5d
jflyhak <1505811266@qq.com>
2020-05-25 13:10:52 +0800

    page of me copmlete!

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -5,14 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl:"",
+    nickName:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                nickName: res.userInfo.nickName
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
