bf5593fa9e6c8c645fe02381d8baa626426d4fcb
jflyhak <1505811266@qq.com>
2020-05-25 10:45:21 +0800

    2020/5/25 10:45

--- miniprogram/pages/create/create.js
+++ miniprogram/pages/create/create.js
@@ -5,8 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
+    footerRight:'footer_right_default',
     accounts: ["京东", "韵达", "申通"],
-        accountIndex: 0,
+    accountIndex: 0,
+    formData:{
+      staff_name:'',
+      goods_number:"",
+      owner_name:"",
+      owner_phone:"",
+      express_type:"京东"
+    }
   },
 
   /**
@@ -65,10 +73,52 @@ Page({
 
   },
   bindAccountChange: function(e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-
+    // console.log('picker account 发生选择改变，携带值为', e.detail.value);
+    let value = e.detail.value;
+    let express;
+   if(value == 0){
+    express="京东"
+   }else if(value ==1){
+    express="韵达"
+   }else if(value==2){
+    express="申通"
+   }
     this.setData({
-        accountIndex: e.detail.value
+        accountIndex: e.detail.value,
+        [`formData.express_type`]:express
     })
+},
+bindInputChange:function(e){
+  console.log(e.target.dataset.field,e.detail.value);
+  this.setData({
+    [`formData.${e.target.dataset.field}`]:e.detail.value
+  })
+  this.watchBtnStatus();
+  // console.log(this.data.formData);
+},
+
+scanCode:function(){
+  let that =this;
+  wx.scanCode({
+    scanType:'barCode',
+    success (res) {
+      // console.log(res.result)
+      that.setData({
+        [`formData.goods_number`]:res.result
+      })
+    }
+  })
+},
+
+watchBtnStatus:function(){
+  if(this.data.formData.goods_number!="" && this.data.formData.owner_name!="" && this.data.formData.owner_phone!=""){
+     this.setData({
+      footerRight:'footer_right_active'
+     })
+  }else{
+    this.setData({
+     footerRight:'footer_right_default'
+    })
+  }
 }
 })
